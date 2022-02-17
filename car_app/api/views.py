3a32f182f55bffc4dff578a25537ad6db5b86572
Pawel Hamera <pjhamera@gmail.com>
2022-02-17 11:23:37 +0100

    removed fitlering from settings and views

--- car_app/api/views.py
+++ car_app/api/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, viewsets
 from rest_framework.exceptions import ValidationError
-from rest_framework import filters
 from ..models import Car, CarRate
 from .serializers import CarSerializer, CarRatingSerializer, CarPopularListSerializer
 
@@ -13,12 +12,11 @@ class CarViewset(viewsets.ModelViewSet):
 
 class CarPopularListViewset(viewsets.ModelViewSet):
     queryset = Car.objects.all()
-    serializer_class = CarPopularListSerializer    
+    serializer_class = CarPopularListSerializer 
               
 class CarRate(viewsets.ModelViewSet):
     queryset = CarRate.objects.all()
     serializer_class = CarRatingSerializer
-    filter_backends = [filters.OrderingFilter]
-    ordering_fields = ['rates_number']
+    
 
 
